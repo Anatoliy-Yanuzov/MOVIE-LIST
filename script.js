ab53e4e29d541bfada3f4ca91d3ce2192e98a7c0
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-10 23:45:42 +0300

    first init

---
+++ script.js
@@ -0,0 +1,45 @@
+let url = 'http://localhost:3030';
+let movieForm = document.getElementById('add-movie-form');
+let registerForm = document.querySelector('#register-form');
+let registerSection = document.querySelector('.register-section');
+
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let data = new FormData(e.currentTarget);
+   
+    // e.currentTarget.reset();
+
+    fetch(`${url}/users/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: data.get('email'),
+            password: data.get('password')
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        saveToken(data.accessToken)
+        console.log(data);
+        registerSection.classList.add('hide')
+    })
+    .catch(err => {
+        console.error(err);
+    })
+});
+
+movieForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(e.currentTarget);
+    console.log(formData.get("title"));
+
+    e.currentTarget.reset();
+});
+
+function saveToken(token){
+    localStorage.setItem('auth_token', token)
+}
