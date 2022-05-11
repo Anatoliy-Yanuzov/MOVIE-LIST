dd8bf918f47805abaeabfc928c21fde4a330ad2f
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-11 14:58:48 +0300

    created "movieForm" to add movies

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let url = 'http://localhost:3030';
 let movieForm = document.getElementById('add-movie-form');
 let registerForm = document.querySelector('#register-form');
+let loginForm = document.querySelector('#login-form');
 let registerSection = document.querySelector('.register-section');
 let addMovieSection = document.querySelector('.add-movie-section');
 
@@ -8,7 +9,7 @@ registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let data = new FormData(e.currentTarget);
-   
+
     e.currentTarget.reset();
 
     fetch(`${url}/users/register`, {
@@ -21,27 +22,85 @@ registerForm.addEventListener('submit', (e) => {
             password: data.get('password')
         })
     })
-    .then(res => res.json())
-    .then(data => {
-        saveToken(data.accessToken)
-        console.log(data);
-        registerSection.classList.add('hide')
-        addMovieSection.classList.remove('hide')
+        .then(res => res.json())
+        .then(data => {
+            saveToken(data.accessToken)
+            console.log(data);
+            registerSection.classList.add('hide')
+            addMovieSection.classList.remove('hide')
+        })
+        .catch(err => {
+            console.error(err);
+        })
+});
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let data = new FormData(e.currentTarget);
+
+    e.currentTarget.reset();
+
+    fetch(`${url}/users/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: data.get('email'),
+            password: data.get('password')
+        })
     })
-    .catch(err => {
-        console.error(err);
-    })
+        .then(res => res.json())
+        .then(data => {
+            saveToken(data.accessToken)
+            console.log(data);
+            registerSection.classList.add('hide')
+            addMovieSection.classList.remove('hide')
+        })
+        .catch(err => {
+            console.error(err);
+        })
 });
 
 movieForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let formData = new FormData(e.currentTarget);
-    console.log(formData.get("title"));
+
+    let title = formData.get('title');
+    let description = formData.get('description');
+    let token = getToken();
+
+    fetch(`${url}/data/movies`, {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify({
+            title,
+            description
+        }),
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log('There is a problem with fetch request');
+        })
 
     e.currentTarget.reset();
 });
 
-function saveToken(token){
+function saveToken(token) {
     localStorage.setItem('auth_token', token)
+}
+
+function getToken() {
+    let token = localStorage.getItem('auth_token');
+
+    return token;
 }
